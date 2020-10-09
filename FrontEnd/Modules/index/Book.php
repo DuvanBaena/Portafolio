819075eb225859c41b66d74bdd682d8e39c486e9
duvan baena <djbpuerta@hotmail.com>
2020-10-09 16:27:51 -0500

    Add Red Me

--- FrontEnd/Modules/index/Book.php
+++ FrontEnd/Modules/index/Book.php
@@ -7,7 +7,7 @@ $records=$data->ListBooks();
           <span>book's</span>
           <h2>Book's</h2>
           <p>
-           Maybe it's useful to you, I would like to recommend some books that have inspired me.</p>
+           Maybe it's useful to you, I would like to recommend you,some books that have inspired me.</p>
         </div>
 
         <div class="row" data-aos="fade-up">
@@ -19,13 +19,14 @@ $records=$data->ListBooks();
           </div>
         </div>
           <div class="row portfolio-container" data-aos="fade-up" data-aos-delay="200">
+
           <?php
               if(count($records)>0){
               for($i=0;$i<count($records);$i++){?>        
             <div class="col-lg-3 col-md-6 portfolio-item filter-app">
               <div class="portfolio-wrap">
                 <figure>
-                  <img src="<?php echo UrlServer?>/FrontEnd/Resources/img/Books/<?php echo $records[$i]["ImgFront"];?>" style="width:255px;height:345px;" class="img-fluid">
+                  <img src="<?php echo UrlServer?>/FrontEnd/Resources/img/Books/<?php echo $records[$i]["ImgFront"];?>" class="img-fluid">
                   <a href="<?php echo UrlServer?>/FrontEnd/Resources/img/Books/<?php echo $records[$i]["ImgBack"];?>" data-lightbox="portfolio" class="link-preview venobox" title="<?php echo $records[$i]["Thematic"];?>"><i class="icofont-eye-alt"></i></a>
                 </figure>
                 <div class="portfolio-info">
---
+++ Read Me Portfolio Web Sites.md
@@ -0,0 +1,41 @@
+# Home
+Author: Duvan Baena Puerta
+Url: https://duvanbaena.com
+
+This is a small personal project that seeks to create its own brand, covering several specific topics, such as.
+
+  - About myself
+  - Education
+  - Work Experiences
+  - Skills
+  - Literature
+  - Testimonials
+  - Areas Interest
+  
+# About myself!
+
+  - Here I talk a little about myself
+
+> Sé un estudiante mientras aún tengas algo que aprende
+> Ralph Waldo Emerson.
+
+This text you see here is *actually* written in Markdown! To get a feel for Markdown's syntax, type some text into the left window and watch the results in the right.
+
+### Skills
+
+Remember that there is no good or bad technology, some are simply of great or little interest
+
+* [FrontEnd] - HTML - CSS - JAVASCRIPT
+* [BackEnd] - PHP
+
+### Installation
+keep in mind the General.php and Connectdb.php file
+
+License
+----
+MIT
+
+**Free Software, Hell Yeah!**
+
+   [FrontEnd]: <http:xxxxx>
+   [Backend]: <http://xxxx>
