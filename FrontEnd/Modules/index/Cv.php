dd235d97f4563ac7dced304a1be068abc96b7e90
duvan baena <djbpuerta@hotmail.com>
2021-10-25 14:12:50 -0500

    Fix TmpDownload CV

--- FrontEnd/Modules/index/Cv.php
+++ FrontEnd/Modules/index/Cv.php
@@ -1,7 +1,9 @@
 <section id="cv" class="cv section-bg">
       <div class="container">
         <div class="text-center">          
-          <button type="button" class="cv-btn"  onclick="DownLoadCV()">Download CV</button>
+          <!-- <button type="button" class="cv-btn"  onclick="DownLoadCV()">Download CV</button>        -->
+          <a class="cv-btn" href="Resources/vendor/Documents/v1Duvan_CV.pdf" download="CV_Duvan.pdf">Download CV</a>
+
         </div>
       </div>
 </section>
--- FrontEnd/Resources/js/script.js
+++ FrontEnd/Resources/js/script.js
@@ -1634,3 +1634,4 @@ function DownLoadCV() {
   });
 }
 
+
