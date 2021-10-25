c530dda9d61b2aea0bfb7149afb29fd44c9f11b4
duvan baena <djbpuerta@hotmail.com>
2021-10-25 14:28:10 -0500

    Fix url DownLoad CV

--- FrontEnd/Modules/index/Cv.php
+++ FrontEnd/Modules/index/Cv.php
@@ -2,7 +2,7 @@
       <div class="container">
         <div class="text-center">          
           <!-- <button type="button" class="cv-btn"  onclick="DownLoadCV()">Download CV</button>        -->
-          <a class="cv-btn" href="Resources/vendor/Documents/v1Duvan_CV.pdf" download="CV_Duvan.pdf">Download CV</a>
+          <a class="cv-btn" href="FrontEnd/Resources/vendor/Documents/v1Duvan_CV.pdf" download="CV_Duvan.pdf">Download CV</a>
 
         </div>
       </div>
